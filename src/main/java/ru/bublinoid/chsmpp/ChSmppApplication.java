c3cf3984ec0d47fcc18e9ae6a6880490273ac107
Hleb <gleb.qb@gmail.com>
2024-09-27 15:21:11 +0200

    init

---
+++ src/main/java/ru/bublinoid/chsmpp/ChSmppApplication.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.chsmpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChSmppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChSmppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bublinoid/chsmpp/ChSmppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.chsmpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChSmppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
